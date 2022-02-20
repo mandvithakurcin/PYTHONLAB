5bf456a06036c4d3759be265cb7a0edbdef8e5d9
mandvithakurcin <91454548+mandvithakurcin@users.noreply.github.com>
2022-02-20 17:11:16 +0530

    Update Create_yearANDdate.py

--- Create_yearANDdate.py
+++ Create_yearANDdate.py
@@ -1,30 +1,6 @@
-date=input("enter date in mm:dd:yyyy ")
+x= int(input())
 
-m=int(date[0:2])
-d=int(date[3:5])
-y=int(date[6:10])
+months = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 
+          7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
 
-if(m==1):
-  print("jan",d,y)
-elif(m==2):
-  print("feb",d,y)
-elif(m==3):
-  print("march",d,y)
-elif(m==4):
-  print("April",d,y)
-elif(m==5):
-  print("may",d,y)
-elif(m==6):
-  print("june",d,y)
-elif(m==7):
-  print("july",d,y)
-elif(m==8):
-  print("August",d,y)
-elif(m==9):
-  print("sept",d,y)
-elif(m==10):
-  print("Oct",d,y)
-elif(m==11):
-  print("Nov",d,y)
-else:
-  print("Dec",d,y)
+print(months[x//10000],(x//100)%100,2000+x%100)
